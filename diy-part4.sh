8600dd00c1a9779207cf668241466c4d31fa1605
wwz09 <47264059+wwz09@users.noreply.github.com>
2022-02-08 18:42:27 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -32,11 +32,6 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/lu
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/YL2209/luci-theme-argon-lr.git package/lean/luci-theme-argon-lr
 
-# '修改默认主题为Argon'
-sed -i 's/option mediaurlbase \/luci-static\/bootstrap/option mediaurlbase \/luci-static\/argon/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/Bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 
 #修改WIFI国家区域
 sed -i 's/US/CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
